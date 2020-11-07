804e4f7f3a60c6a9e1cb5e895e5e7678131da2c1
Faizan <faizanhuda12@gmail.com>
2020-11-07 16:34:51 -0500

    Updates

---
+++ precache-manifest.c2153a79d76314674f257012544ddc8d.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e55843440ecab6521d08ecef399c29",
+    "url": "/portfolio-web/index.html"
+  },
+  {
+    "revision": "ce5ef26a574d1e249703",
+    "url": "/portfolio-web/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "f9a6fe0754e507752d2b",
+    "url": "/portfolio-web/static/css/main.bc24b4e5.chunk.css"
+  },
+  {
+    "revision": "ce5ef26a574d1e249703",
+    "url": "/portfolio-web/static/js/2.0c77e4ec.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "/portfolio-web/static/js/2.0c77e4ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9a6fe0754e507752d2b",
+    "url": "/portfolio-web/static/js/main.d3ed2b1b.chunk.js"
+  },
+  {
+    "revision": "3864ea7cc6dcb51d1ef7",
+    "url": "/portfolio-web/static/js/runtime-main.be154a11.js"
+  },
+  {
+    "revision": "b1003b8f4a8f5dad28411d0ae289d868",
+    "url": "/portfolio-web/static/media/data.b1003b8f.jpg"
+  },
+  {
+    "revision": "949f9913f63c13970c79451d11e88682",
+    "url": "/portfolio-web/static/media/dog.949f9913.jpg"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/portfolio-web/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "dff068bc8abc626833ae5156acd71c22",
+    "url": "/portfolio-web/static/media/expense.dff068bc.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio-web/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio-web/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio-web/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio-web/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio-web/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "2bed4d84ad932f36718b10b1eb42be77",
+    "url": "/portfolio-web/static/media/hollow.2bed4d84.jpg"
+  },
+  {
+    "revision": "f897753c056b8f181e9658bd962d90e7",
+    "url": "/portfolio-web/static/media/house.f897753c.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/portfolio-web/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/portfolio-web/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/portfolio-web/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/portfolio-web/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "a82ab1cd3c28c0a07a4932d57176cb88",
+    "url": "/portfolio-web/static/media/male.a82ab1cd.png"
+  },
+  {
+    "revision": "3ec3891b37f8b0765b554f10806f0b3f",
+    "url": "/portfolio-web/static/media/male1.3ec3891b.png"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/portfolio-web/static/media/myImage.bd80794f.png"
+  },
+  {
+    "revision": "4d9b926c2689da05e978f49864a6e1ff",
+    "url": "/portfolio-web/static/media/sort.4d9b926c.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/faizanhuda12/portfolio-web/precache-manifest.9b9aac8c1702e959cde2bf268c90eaf0.js"
+  "/portfolio-web/precache-manifest.c2153a79d76314674f257012544ddc8d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/faizanhuda12/portfolio-web/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
